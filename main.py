72696d8898e08f26d370f5948fbd0525f407982f
MNastri <marcellonastri@gmail.com>
2021-08-16 16:48:25 -0300

    Criar dicionário para os numerais romanos

--- main.py
+++ main.py
@@ -60,6 +60,14 @@ from typing import List
 
 class Solution:
     """ Solução do problema. Não precisa inicializar"""
+    dict_roman = {'I': 1,
+                  'V': 5,
+                  'X': 10,
+                  'L': 50,
+                  'C': 100,
+                  'D': 500,
+                  'M': 1000}
+
     def roman_to_int(self, s: str) -> int:
         return 0
 
