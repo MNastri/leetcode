fbbd2202a4f44709c72c8e2d573256fe1304482e
MNastri <marcellonastri@gmail.com>
2021-08-21 13:36:00 -0300

    Incluir comentário sobre a lógica usada

--- main.py
+++ main.py
@@ -27,22 +27,31 @@ class Solution:
     """ Solução do problema. Não precisa inicializar a classe"""
     def longest_common_prefix(self, strs: List[str]) -> str:
         if 0 == len(strs):
-            return ""
+            return ''
         if 1 == len(strs):
             return strs[0]
         return_str: str = strs[0]
 
         # for first_item in strs:
-        #     for second_item in strs[1:]:
-        #         print(f'len(return_strs)=={len(return_str)}')
+        #     for second_item in strs[strs.index(first_item)+1:]:
+        #         # print(f'f({first_item})', end='')
+        #         print(f', s({second_item})', end='')
+        #         # print(f', ret({return_str})', end='')
+        #         # print(f', len(return_strs)=={len(return_str)}', end='')
+        #         print('', end='. ')
         #         if 0 == len(return_str):
-        #             return ""
-        #         return_str = return_str[:1]
+        #             return ''
+        #         return_str = return_str[:]
         #         pass
-        # comparar itens dois a dois
-        # nos itens, comparar letra a letra?
-        # já tenho uma string de retorno, como comparar string com string?
-        # como reduzir o tamanho da string de retorno caso não haja o prefixo nas duas strings?
+
+        # ideias:
+        # salvar no string de retorno a primeira string
+        # para as strings do input que não sejam a primeira
+        # para os índices da string de retorno
+        # comparar a string de retorno, até o índice usado, com ...
+        # ...o prefixo, usando o mesmo índice, da string do input usado
+        # se ret e pref forem iguais, continue
+        # se ret e pref forem diferentes, retirar caracteres extras de ret
         # quando a string de retorno tiver tamanho zero, retornar ""
         return return_str
 
@@ -55,11 +64,16 @@ def test(input_strs: List[str]) -> None:
 
 
 def main_loop():
-    test([])                            #output:""
-    test(['testing'])                   #output:"testing"
-    test(['flower', 'flow', 'flight'])  #output:"fl"
-    test(['dog', 'racecar', 'car'])     #output:""
-
+    test([])                            # output:""
+    test(['testing'])                   # output:"testing"
+    test(['a', 'ab', 'abc'])            # output:"a"
+    test(['a', 'abc', 'ab'])            # output:"a"
+    test(['ab', 'a', 'abc'])            # output:"a"
+    test(['ab', 'abc', 'a'])            # output:"a"
+    test(['abc', 'a', 'ab'])            # output:"a"
+    test(['abc', 'ab', 'a'])            # output:"a"
+    test(['flower', 'flow', 'flight'])  # output:"fl"
+    test(['dog', 'racecar', 'car'])     # output:""
 
 
 if __name__ == '__main__':
