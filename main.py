b731a8c2ea7814271b8d008eebea435ff5edda69
MNastri <marcellonastri@gmail.com>
2021-08-26 20:12:28 -0300

    Preparar arquivo com testes

--- main.py
+++ main.py
@@ -39,10 +39,10 @@ Example 2:
 
 Input: nums = [0,0,1,1,1,2,2,3,3,4]
 Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
-Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+Explanation: Your function should return k = 5, with the first five elements of nums being 0,
+1, 2, 3, and 4 respectively.
 It does not matter what you leave beyond the returned k (hence they are underscores).
 
-Constraints:
 Constraints:
 
 0 <= nums.length <= 3 * 10**4
@@ -53,82 +53,24 @@ nums is sorted in non-decreasing order.
 from typing import List, Optional
 
 
-class ListNode:
-    """ Nodos para uma lista linkada."""
-    def __init__(self, value=0, nxt=None):
-        self.val = value
-        self.next = nxt
-
-    def print_val(self):
-        items = []
-        items.append(self.val)
-        if self.next == None:
-            return
-        node = self.next
-        while node:
-            items.append(node.val)
-            node = node.next
-        print(f'{items}')
-
-
 class Solution:
-    """ Solução do problema. Não precisa inicializar a classe"""
-    def merge_two_lists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        """
+    """ Solução do problema. Não precisa inicializar a classe."""
+    def removeDuplicates(self, nums: List[int]) -> int:
+        return -1
 
-        :param l1:
-        :param l2:
-        :return:
-        """
-        tail = dummy = ListNode()
-        # l1.print_val()
-        # l2.print_val()
-        while l1 and l2:
-            if l1.val <= l2.val:
-                tail.next = l1
-                l1 = l1.next
-            else:
-                tail.next = l2
-                l2 = l2.next
-            tail = tail.next
-        if l1:
-            tail.next = l1
-        elif l2:
-            tail.next = l2
-        # dummy.print_val()
-        return dummy.next
-
-
-def test(ln1: ListNode, ln2: ListNode) -> None:
+def test(nums:List[int]) -> None:
     """ Teste o input na solução."""
-    print(f'\nInput:{ln1},{ln2}', end='. ')
+    print(f'\nInput: nums = {nums}', end='. ')
     a = Solution()
-    print(f'Output:{a.merge_two_lists(ln1, ln2)}', end='. ')
+    print(f'Output: {a.removeDuplicates(nums)}', end=', ')
+    print(f'nums = {nums}', end='. ')
 
 
 def main_loop():
-    # implementar testes com ListNode que ainda não tenho certeza de como fazer
-    ln1 = ListNode(4)
-    ln1 = ListNode(2, ln1)
-    ln1 = ListNode(1, ln1)
-
-    ln2 = ListNode(4)
-    ln2 = ListNode(3, ln2)
-    ln2 = ListNode(1, ln2)
-
-    test(ln1, ln2)
-
-    # ln1 = ListNode([1, 2, 4])
-    # ln2 = ListNode([1, 3, 4])
-    # test(ln1, ln2)
-    #
-    # ln1 = ListNode([])
-    # ln2 = ListNode([])
-    # test(ln1, ln2)
-    #
-    # ln1 = ListNode([])
-    # ln2 = ListNode([0])
-    # test(ln1, ln2)
+    nums = [1, 1, 2]
+    test(nums)
+    nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    test(nums)
 
 
 if __name__ == '__main__':
