39ae987d6743efc1d5bd9e968eba6b387bacd9f3
MNastri <marcellonastri@gmail.com>
2022-03-23 23:53:49 -0300

    fix:get right side of an array

--- problems/p0004_2_median_of_two_sorted_arrays/main.py
+++ problems/p0004_2_median_of_two_sorted_arrays/main.py
@@ -17,9 +17,27 @@ def median(array: List[int]) -> float:
     return array[int((ll - 1) / 2)]
 
 
-# if __name__ == "__main__":
-#     arr = [0, 7, 35, 42]
-#     print(median(array=arr))
+if __name__ == "__main__":
+    arr = [0, 7, 39, 42]
+    print(median(array=arr))
+    arr1 = [0, 7, 23, 39, 42]
+    print(median(array=arr1))
+
+
+def get_upper_half_of(array):
+    ll = len(array)
+    if ll == 1:
+        return array
+    if ll % 2 == 0:
+        return array[int(ll / 2) :]
+    return array[int(((ll - 1) / 2)) :]
+
+
+if __name__ == "__main__":
+    arr = [0, 7, 39, 42]
+    print(get_upper_half_of(array=arr))
+    arr1 = [0, 7, 23, 39, 42]
+    print(get_upper_half_of(array=arr1))
 
 
 def merge_arrays(array1: List[int], array2: List[int]) -> List[int]:
