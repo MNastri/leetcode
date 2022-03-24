2076f9d2d83e2c749a80c6f9a1ff23ec20434114
MNastri <marcellonastri@gmail.com>
2022-03-24 01:49:21 -0300

    feat:get elements around median

--- problems/p0004_2_median_of_two_sorted_arrays/main.py
+++ problems/p0004_2_median_of_two_sorted_arrays/main.py
@@ -6,21 +6,45 @@ class Solution:
         pass
 
 
+def left_of_median(array: List[int]) -> int:
+    ll = len(array)
+    if ll % 2 == 0:
+        return array[int(ll / 2 - 1)]
+    raise TypeError("array should have even size")
+
+
+if __name__ == "__main__":
+    arr = [0, 7, 39, 42]
+    print(left_of_median(array=arr))
+
+
+def right_of_median(array: List[int]) -> int:
+    ll = len(array)
+    if ll % 2 == 0:
+        return array[int(ll / 2)]
+    raise TypeError("array should have even size")
+
+
+if __name__ == "__main__":
+    arr = [0, 7, 39, 42]
+    print(right_of_median(array=arr))
+
+
 def median(array: List[int]) -> float:
     ll = len(array)
     if ll == 1:
         return array[0]
     if ll % 2 == 0:
-        left_of_median = array[int(ll / 2 - 1)]
-        right_of_median = array[int(ll / 2)]
-        return (left_of_median + right_of_median) / 2
+        left = left_of_median(array=array)
+        right = right_of_median(array=array)
+        return (left + right) / 2
     return array[int((ll - 1) / 2)]
 
 
 if __name__ == "__main__":
     arr = [0, 7, 39, 42]
     print(median(array=arr))
-    arr1 = [0, 7, 23, 39, 42]
+    arr1 = [0, 7, 25, 39, 42]
     print(median(array=arr1))
 
 
