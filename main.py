a80dd341434e7872143257118bfa68974b1d661b
MNastri <marcellonastri@gmail.com>
2022-03-26 16:55:07 -0300

    feat:count problems, get easiness ratios

--- main.py
+++ main.py
@@ -35,12 +35,30 @@ def get_problems_by_difficulty() -> t.List[t.List[int]]:
     return solved_problems
 
 
+def count_problems() -> t.List[int]:
+    """Returns the count of problems in each difficulty."""
+    solved_problems = get_problems_by_difficulty()
+    counts = [ll for ll in map(len, solved_problems)]
+    return counts
+
+
+def get_easiness_ratios() -> t.List[float]:
+    """In relation to the hardest difficulty."""
+    counts = count_problems()
+    ratios = []
+    for diff in counts[:-1]:
+        if diff != 0:
+            ratios.append(diff / counts[-1])
+    return ratios
+
+
 if __name__ == "__main__":
     solved = get_problems_by_difficulty()
-    counts = map(len, solved)
-    biggest_count = max(counts)
+    biggest_count = max(count_problems())
     charact_of_count = len(str(biggest_count))
-    whitespace = " "*charact_of_count
+    whitespace = " " * charact_of_count
     for idx, diff in enumerate(solved):
         len_of_diff = (whitespace + str(len(diff)))[-charact_of_count:]
         print(f"Dificuldade {idx} - Problemas ({len_of_diff}): {diff}")
+    diff_ratios = get_easiness_ratios()
+    print(f"Proporção de facilidade: {diff_ratios}")
