86365272336332b7d85b4b9cd1b2ed76f099fe36
MNastri <marcellonastri@gmail.com>
2022-03-30 10:43:56 -0300

    feat:remove all chars but upper/lowercase ones

--- problems/p0125_1_valid_palindrome/main.py
+++ problems/p0125_1_valid_palindrome/main.py
@@ -1,5 +1,16 @@
 """Checks if a phrase is a palindrome."""
+from string import ascii_lowercase
+
+
+def is_palindrome(s):
+    s = "".join([c.lower() for c in s if c.lower() in ascii_lowercase])
+    print(s)
+
+
 if __name__ == "__main__":
     input = "A man, a plan, a canal: Panama"
+    is_palindrome(input)
     input = "race a car"
+    is_palindrome(input)
     input = " "
+    is_palindrome(input)
